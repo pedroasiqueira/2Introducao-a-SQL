470362a4af360b678c0a7682fce9998686114197
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-06-28 10:11:39 -0300

    feat: criando pasta e arquivo js

---
+++ dia-2/commands.js
@@ -0,0 +1,14 @@
+// SELECT * FROM sakila.city;
+// SELECT first_name, last_name FROM customer;
+// SELECT * FROM sakila.rental;
+// SELECT title, description, release_year FROM sakila.film;
+// SELECT * FROM sakila.country;
+
+
+
+// SELECT first_name, last_name FROM sakila.actor;
+// SELECT CONCAT(first_name," ", last_name) AS "Nome Completo" FROM sakila.actor;
+
+// SELECT CONCAT(title, ", ", release_year) AS "Lançamento do Filme" FROM sakila.film;
+// SELECT CONCAT(address, ", ", district) AS "Endereço" FROM sakila.address;
+// SELECT * FROM sakila.address;
