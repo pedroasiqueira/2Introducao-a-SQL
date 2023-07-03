d36f77766722e8c7bfda9029c74d63c49f7d0316
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-07-03 18:14:27 -0300

    fix: adc BETWEEN

--- Dia-3/commands-dia3.js
+++ Dia-3/commands-dia3.js
@@ -125,4 +125,14 @@ O que tiver dentro dos parenteses será "resolvido" primeiro.
     Trazendo resultas de números entre 50 e 120
     SELECT title, length FROM sakila.film 
     WHERE length BETWEEN 50 AND 120;
+
+    Usando BETWEEN com strings
+    SELECT * FROM sakila.language 
+    WHERE name BETWEEN 'Italian' AND 'Mandarin' 
+    ORDER BY name;
+
+    Usando BETWEEN com datas
+    SELECT rental_id, rental_date FROM sakila.rental 
+    WHERE rental_date 
+    BETWEEN '2005-05-27' AND '2005-07-17';
  */
